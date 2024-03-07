e45ab1339955ac1d376f437abe29c9943192aa7b
rathod125 <tusharrathod5801@gmail.com>
2024-03-06 16:52:42 -0800

    commit 3

--- TestClass/Class1.cs
+++ TestClass/Class1.cs
@@ -80,7 +80,109 @@ namespace TestClass
                 Assert.That(expected, Is.EqualTo(actual));
 
             }
-            
+            [Test]
+            public void ValidScaleneTriangle_Input30and40and50_OutputValidScaleneTriangle()
+            {
+                //Arrange
+                int firstSide = 30;
+                int secondSide = 40;
+                int thirdSide = 50;
+
+                string expected = "Scalene triangle";
+
+                //Act
+                string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+                //Assert
+                Assert.That(expected, Is.EqualTo(actual));
+
+            }
+            [Test]
+            public void ValidScaleneTriangle_Input30and50and40_OutputValidScaleneTriangle()
+            {
+                //Arrange
+                int firstSide = 30;
+                int secondSide = 50;
+                int thirdSide = 40;
+
+                string expected = "Scalene triangle";
+
+                //Act
+                string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+                //Assert
+                Assert.That(expected, Is.EqualTo(actual));
+
+            }
+            [Test]
+            public void ValidScaleneTriangle_Input40and30and50_OutputValidScaleneTriangle()
+            {
+                //Arrange
+                int firstSide = 40;
+                int secondSide = 30;
+                int thirdSide = 50;
+
+                string expected = "Scalene triangle";
+
+                //Act
+                string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+                //Assert
+                Assert.That(expected, Is.EqualTo(actual));
+
+            }
+            [Test]
+            public void ValidScaleneTriangle_Input40and50and30_OutputValidScaleneTriangle()
+            {
+                //Arrange
+                int firstSide = 40;
+                int secondSide = 50;
+                int thirdSide = 30;
+
+                string expected = "Scalene triangle";
+
+                //Act
+                string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+                //Assert
+                Assert.That(expected, Is.EqualTo(actual));
+
+            }
+            [Test]
+            public void ValidScaleneTriangle_Input50and40and30_OutputValidScaleneTriangle()
+            {
+                //Arrange
+                int firstSide = 50;
+                int secondSide = 40;
+                int thirdSide = 30;
+
+                string expected = "Scalene triangle";
+
+                //Act
+                string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+                //Assert
+                Assert.That(expected, Is.EqualTo(actual));
+
+            }
+            [Test]
+            public void ValidScaleneTriangle_Input50and30and40_OutputValidScaleneTriangle()
+            {
+                //Arrange
+                int firstSide = 50;
+                int secondSide = 30;
+                int thirdSide = 40;
+
+                string expected = "Scalene triangle";
+
+                //Act
+                string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+                //Assert
+                Assert.That(expected, Is.EqualTo(actual));
+
+            }
+
         }
     }
 }
